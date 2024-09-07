665da1cef0253a106b9f2f78210e96d93333ce76
Hasibul Hamid <hamidhasibul@gmail.com>
2024-09-07 21:49:10 +0600

    feat: add expense

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"os"
+	"time"
 )
 
 type Expense struct {
@@ -34,6 +37,45 @@ func main() {
 	switch cmd {
 	case "help":
 		println("help - list of commands")
+	case "add":
+		addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+		descPtr := addCmd.String("description", "description", "expense description")
+		amountPtr := addCmd.Int("amount", 0, "expense amount")
+		addCmd.Parse(os.Args[2:])
+
+		var newId int
+
+		if len(expenses) > 0 {
+			newId = expenses[len(expenses)-1].Id + 1
+		} else {
+			newId = 1
+		}
+
+		newExpense := Expense{
+			Id:          newId,
+			Date:        time.Now().Format("2006-01-02"),
+			Description: *descPtr,
+			Amount:      *amountPtr,
+		}
+
+		expenses = append(expenses, newExpense)
+
+		updatedData, err := json.MarshalIndent(expenses, "", " ")
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.WriteFile("./data.json", updatedData, 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Expense added successfully (ID: %d)\n", newId)
+
+		// TODO: List Expenses
+		// TODO: Delete expense by Id
+		// TODO: Summerise total Expense
+		// TODO: Summerise total Expense (by Month)
 
 	}
 }
